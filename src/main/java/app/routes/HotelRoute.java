3676cced8d07557ecc143a2cb6d60c59654fb4f8
AlissaStorm <Alissaalidib@hotmail.com>
2025-10-01 13:36:38 +0200

    In progress of testing endpoints

--- src/main/java/app/routes/HotelRoute.java
+++ src/main/java/app/routes/HotelRoute.java
@@ -1,12 +1,14 @@
 package app.routes;
 
 import app.controllers.HotelController;
+import app.controllers.RoomController;
 import io.javalin.apibuilder.EndpointGroup;
 import static io.javalin.apibuilder.ApiBuilder.*;
 
 public class HotelRoute {
 
     private HotelController hotelController = new HotelController();
+    private RoomController roomController = new RoomController();
 
     public EndpointGroup getRoutes() {
         return () -> {
@@ -16,6 +18,13 @@ public class HotelRoute {
             post("/", hotelController::createHotel);
             put("/{id}", hotelController::updateHotel);
             delete("/{id}", hotelController::deleteHotel);
+
+            // Room endpoints under et hotel
+            path("/{hotelId}/rooms", () -> {
+                get(hotelController::getRoomsForHotel);
+                post(roomController::addRoomToHotel);
+                delete("/{roomId}", roomController::removeRoomFromHotel);
+            });
         };
     }
 
